7461ad06b5130cb2f3642f4cbd2fc16067707373
Maksuda Mila <afraazra171@gmail.com>
2022-02-03 17:40:00 +0600

    Add Code

--- practice-app/src/App.js
+++ practice-app/src/App.js
@@ -1,7 +1,10 @@
+import ClockList from "./component/ClockList";
+
 function App() {
+  const quantities = [1, 2, 3];
   return (
-    <div className="App">
-    <h1>Hello</h1>
+    <div>
+      <ClockList quantities={quantities} />
     </div>
   );
 }
